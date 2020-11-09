122779697d006f35a33e65d25ca6ddbe8036a6b9
emilybruns <emilybruns@gmail.com>
2020-11-09 14:06:18 -0800

    blogging pushed.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
 # and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
